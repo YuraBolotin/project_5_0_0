7cc7be72c9f74add89a0858990b1d3240be5a80c
Yura <bolotinya@mail.ru>
2022-03-16 22:11:34 +0300

    Доработанное домашнее задание "массивы"

--- src/com/bolotin/Main.java
+++ src/com/bolotin/Main.java
@@ -15,16 +15,56 @@ public class Main {
         int[] arr3 = new int[]{2, 4, 6};
 
         // Задача 2
+        System.out.println("Задача 2");
 
-        System.out.println(arr[0] + ", " + arr[1] + ", " + arr[2]);
-        System.out.println(arr2[0] + ", " + arr2[1] + ", " + arr2[2]);
-        System.out.println(arr3[0] + ", " + arr3[1] + ", " + arr3[2]);
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == arr[arr.length - 1]) {
+                System.out.println(arr[i]);
+            } else {
+                System.out.print(arr[i] + ", ");
+            }
+        }
+
+            for (int i = 0; i < arr2.length; i++) {
+                if (arr2[i] == arr2[arr2.length - 1]) {
+                    System.out.println(arr2[i]);
+                } else {
+                    System.out.print(arr2[i] + ", ");
+                }
+        }
+        for (int i = 0; i < arr3.length; i++) {
+            if (arr3[i] == arr3[arr.length - 1]) {
+                System.out.println(arr3[i]);
+            } else {
+                System.out.print(arr3[i] + ", ");
+            }
+        }
 
         // Задача 3
+        System.out.println("Задача 3");
 
-        System.out.println(arr[2] + ", " + arr[1] + ", " + arr[0]);
-        System.out.println(arr2[2] + ", " + arr2[1] + ", " + arr2[0]);
-        System.out.println(arr3[2] + ", " + arr3[1] + ", " + arr3[0]);
+        for (int i = arr.length - 1; i >= 0; i --) {
+            if (i == 0) {
+                System.out.println(arr[i]);
+            } else {
+                System.out.print(arr[i] + ", ");
+            }
+        }
+
+        for (int i = arr2.length - 1; i >= 0; i --) {
+            if (i == 0) {
+                System.out.println(arr2[i]);
+            } else {
+                System.out.print(arr2[i] + ", ");
+            }
+        }
+        for (int i = arr3.length - 1; i >= 0; i --) {
+            if (i == 0) {
+                System.out.println(arr3[i]);
+            } else {
+                System.out.print(arr3[i] + ", ");
+            }
+        }
 
         // Задача 4
         System.out.println("Задача 4");
